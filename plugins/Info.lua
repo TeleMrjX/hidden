2cbbbf74444eedc8f81ebd752ce783360326f585
SeyedKia <seyedalikia27@gmail.com>
2017-02-13 16:26:01 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -5,7 +5,7 @@ local img = 'https://api.telegram.org/file/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-
 local res, code = https.request(db)
 local jdat = json:decode(res)
 if jdat.result == nil then
-   return "nil"     
+   reply_msg(msg.id, "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» تعداد عکس ها : "..jdat.result.total_count.."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n", ok_cb, false)    
  else
  local fileid = jdat.result.photos[1][3].file_id
  local pt, code = https.request(path..fileid)
