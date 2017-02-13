92cd64eeca0b80d351f86604997d34fed0317497
SeyedKia <seyedalikia27@gmail.com>
2017-02-13 16:00:27 +0330

    Update BanHammer.lua

--- plugins/BanHammer.lua
+++ plugins/BanHammer.lua
@@ -13,7 +13,13 @@ local function pre_process(msg)
       print('User is banned!')
       local print_name = user_print_name(msg.from):gsub("‮", "")
 	  local name = print_name:gsub("_", "")
-      savelog(msg.to.id, name.." ["..msg.from.id.."] is banned and kicked ! ")-- Save to logs
+      --savelog(msg.to.id, name.." ["..msg.from.id.."] is banned and kicked ! ")-- Save to logs
+      if msg.from.username ~= nil then
+	name = msg.from.username
+       else
+	name = msg.from.first_name				
+      end					
+      reply_msg(msg.id, "⭕️ کاربر "..name.." از گروه محروم است و اخراج شد !", ok_cb, false)				
       kick_user(user_id, msg.to.id)
       end
     end
