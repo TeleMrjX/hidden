c5dd9948360e2dbcf4dafa179784bdabb8bfb428
SeyedKia <seyedalikia27@gmail.com>
2017-02-13 18:40:03 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -7,12 +7,13 @@ local jdat = json:decode(res)
 if jdat.result == nil then
    reply_msg(msg.id, "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n", ok_cb, false)    
  else
+print(serpent.block(jdat.result))      
  local fileid = jdat.result.photos[1][3].file_id
  local pt, code = https.request(path..fileid)
  local jdat2 = json:decode(pt)
  local path2 = jdat2.result.file_path  
  local link = img..path2
- local photo = download_to_file(link,"ax"..msg.from.id..".jpg")
+ local photo = download_to_file(link ,"ax"..msg.from.id..".jpg")
  local caption = "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» تعداد عکس ها : "..jdat.result.total_count.."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n"     
  send_photo2(get_receiver(msg), photo, caption, ok_cb, false)       
 end        
