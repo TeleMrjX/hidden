882358612c0f05daace2b680af2c573bc3e67588
SeyedKia <seyedalikia27@gmail.com>
2017-02-13 13:45:50 +0330

    Update BanHammer.lua

--- plugins/BanHammer.lua
+++ plugins/BanHammer.lua
@@ -135,6 +135,7 @@ end
 local function Kick_reply(extra, success, result)
 	if type(result) == 'boolean' then
 		print('This is a old message!')
+		reply_msg(extra.msg.id, "tesT", ok_cb, true)
 		return false
 	end
         channel_kick('channel#id'..result.to.peer_id, 'user#id'..result.from.peer_id, ok_cb, false)
