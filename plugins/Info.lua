bef19ff126049e462f019b3768998fd140847aaf
SeyedKia <seyedalikia27@gmail.com>
2017-02-13 16:27:11 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -5,7 +5,7 @@ local img = 'https://api.telegram.org/file/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-
 local res, code = https.request(db)
 local jdat = json:decode(res)
 if jdat.result == nil then
-   reply_msg(msg.id, "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» تعداد عکس ها : "..jdat.result.total_count.."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n", ok_cb, false)    
+   reply_msg(msg.id, "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n", ok_cb, false)    
  else
  local fileid = jdat.result.photos[1][3].file_id
  local pt, code = https.request(path..fileid)
@@ -13,7 +13,7 @@ if jdat.result == nil then
  local path2 = jdat2.result.file_path  
  local link = img..path2
  local photo = download_to_file(link,"ax"..msg.from.id..".jpg")
- local caption = "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n"     
+ local caption = "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» تعداد عکس ها : "..jdat.result.total_count.."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n"     
  send_photo2(get_receiver(msg), photo, caption, ok_cb, false)       
 end        
 end
