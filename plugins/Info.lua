d1d7494435816d92bf088d7ff96e2c3b9ba4719b
SeyedKia <seyedalikia27@gmail.com>
2017-02-13 18:46:14 +0330

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -1,27 +1,34 @@
-function run(msg, matches , result)
-local db = 'https://api.telegram.org/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4/getUserProfilePhotos?user_id='..msg.from.id
-local path = 'https://api.telegram.org/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4/getFile?file_id='  
-local img = 'https://api.telegram.org/file/bot236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4/'
-local res, code = https.request(db)
-local jdat = json:decode(res)
-if jdat.result == nil then
-   reply_msg(msg.id, "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n", ok_cb, false)    
- else
-print(serpent.block(jdat.result))      
- local fileid = jdat.result.photos[1][2].file_id
- local pt, code = https.request(path..fileid)
- local jdat2 = json:decode(pt)
- local path2 = jdat2.result.file_path  
- local link = img..path2
- local photo = download_to_file(link ,"ax"..msg.from.id..".jpg")
- local caption = "» نام : "..msg.from.first_name.."\n» شناسه : "..msg.from.id.."\n» نام کاربری : "..("@"..msg.from.username or '--').."\n» تعداد عکس ها : "..jdat.result.total_count.."\n» نام گروه : "..msg.to.title.."\n» شناسه گروه : "..msg.to.id.."\n"     
- send_photo2(get_receiver(msg), photo, caption, ok_cb, false)       
-end        
+function run(msg,matches , result)
+    local user_id = msg.from.id
+    local chat_id = get_receiver(msg)
+    local token = "236823773:AAHuvA1wudU3hStv2Qq4RjD-MtSZoiTRPf4"
+    local db = 'https://api.telegram.org/bot'..token..'/getUserProfilePhotos?user_id='..user_id
+      local path = 'https://api.telegram.org/bot'..token..'/getFile?file_id='
+      local img = 'https://api.telegram.org/file/bot'..token..'/'
+    local res, code = https.request(db)
+      local jdat = json:decode(res)
+      local fileid = jdat.result.photos[1][3].file_id
+    local count = jdat.result.total_count
+    if tonumber(count) == 0 then
+      send_large_msg(chat_id,"Image Not Found",ok_cb,false)
+    else
+      local pt, code = https.request(path..fileid)
+        local jdat2 = json:decode(pt)
+        local path2 = jdat2.result.file_path
+    local link = img..path2
+    local photo = download_to_file(link,"ax"..user_id..".jpg")
+    send_photo2(chat_id, photo, "- نام : "..msg.from.first_name.."\n"
+.."- آیدی : "..msg.from.id.."\n"
+.."- نام کاربری : @"..msg.from.username.."\n"
+.."- نام گروه : "..msg.to.title.."\n"
+.."️- کانال :  \n@TeleGold_Team", ok_cb, false)
+return 
+end
 end
 return {
 patterns = {
-"^([Ii][Dd])$",
-"^(شناسه)$",      
+"^(عکس من)$",
+"^(پروفایل من)$"
 },
 run = run
 }
