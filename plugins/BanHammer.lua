b03a91942af0a0307efa1dbb135b754501621a49
SeyedKia <seyedalikia27@gmail.com>
2017-02-19 20:04:25 +0330

    Update BanHammer.lua

--- plugins/BanHammer.lua
+++ plugins/BanHammer.lua
@@ -45,16 +45,22 @@ local function pre_process(msg)
       end
      if data[tostring(msg.to.id)] then
        if data[tostring(msg.to.id)]['settings'] then
-         if data[tostring(msg.to.id)]['settings']['lock_bots'] then
-           bots_protection = data[tostring(msg.to.id)]['settings']['lock_bots']
+         if data[tostring(msg.to.id)]['settings']['lock_bot'] then
+           bots_protection = data[tostring(msg.to.id)]['settings']['lock_bot']
           end
         end
       end
     if msg.action.user.username ~= nil then
       if string.sub(msg.action.user.username:lower(), -3) == 'bot' and not is_momod(msg) and bots_protection == "yes" then --- Will kick bots added by normal users
-          local print_name = user_print_name(msg.from):gsub("‮", "")
-		  local name = print_name:gsub("_", "")
+          --local print_name = user_print_name(msg.from):gsub("‮", "")
+		  --local name = print_name:gsub("_", "")
           --savelog(msg.to.id, name.." ["..msg.from.id.."] added a bot > @".. msg.action.user.username)-- Save to logs
+	 if msg.from.usrname ~= nil then
+	   uname = '@'..msg.from.username
+	   else
+	   uname = msg.from.first_name..' ['..msg.from.id..']'					
+	 end					
+	  reply_msg(msg.id, '⚠️ کاربر '..uname..' ربات @'..msg.action.user.username..' را عضو کرد !', ok_cb, false)				
           kick_user(msg.action.user.id, msg.to.id)
       end
     end
