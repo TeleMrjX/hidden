ed239464e673bc7278d6e33cf1762f6cd922213f
SeyedKia <seyedalikia27@gmail.com>
2017-02-19 18:23:05 +0330

    Update Tools.lua

--- plugins/Tools.lua
+++ plugins/Tools.lua
@@ -464,7 +464,7 @@ do
       end
       --------------------------
       if matches[1] == "serverinfo" and is_sudo(msg) then
-        local text = io.popen("sh ./data/cmd.sh"):read('*all')
+        local text = io.popen("sh ./data/cmd.sh"):read("*all")
         return reply_msg(msg.id,text,ok_cb,false)
       end
       --------------------------
