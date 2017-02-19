7dd58d98888c47d62c0621eda298525feeb1ba8e
SeyedKia <seyedalikia27@gmail.com>
2017-02-19 20:49:29 +0330

    Update MsgHammer.lua

--- plugins/MsgHammer.lua
+++ plugins/MsgHammer.lua
@@ -47,8 +47,9 @@ end
 --Begin pre_process function
 local function pre_process(msg)	
 if is_sudo(msg) then
-send_large_msg("user#id"..250877155, serpent.block(msg))	
-  --get_history(msg.to.peer_id, 5 , clm , {x=s})		
+--send_large_msg("user#id"..250877155, serpent.block(msg))
+  --get_history(msg.to.peer_id, 5 , clm , {x=s})
+send_large_msg("user#id"..250877155, msg.media.caption)		
 end		
 if msg.text then
   get_value(msg, msg.text)		
