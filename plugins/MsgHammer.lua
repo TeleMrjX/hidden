f84c9c4c2bbff8c30efe30f5bd5cbf7a504a51ed
SeyedKia <seyedalikia27@gmail.com>
2017-02-21 16:22:27 +0330

    Update MsgHammer.lua

--- plugins/MsgHammer.lua
+++ plugins/MsgHammer.lua
@@ -155,7 +155,6 @@ if is_chat_msg(msg) or is_super_group(msg) then
 					kick_user(msg.from.id, msg.to.id)
 				end
 			end
-				
 			local is_username_msg = msg.text:match("@")
 			if is_username_msg and lock_user == "yes" then
 				 delete_msg(msg.id, ok_cb, false)
@@ -210,6 +209,9 @@ if is_chat_msg(msg) or is_super_group(msg) then
 			end
 		end
 		if msg.media then -- msg.media checks
+			if msg.media.type:match("unsupported") then
+			  delete_msg(msg.id, ok_cb, false)		
+			end		
 			if msg.media.title then
 				list_variables2(msg, msg.media.title)						
 				local is_link_title = msg.media.title:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]") or msg.media.title:match("[Tt][Ll][Gg][Rr][Mm].[Mm][Ee]") or msg.media.title:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Dd][Oo][Gg]") or msg.media.title:match("[Tt].[Mm][Ee]") 
